daf4c862403b68b22689f82bcd6fdbc3e8f04bb5
Александр Бухарин <buharinzxc123@gmail.com>
2022-03-04 14:46:02 +0500

    + wallet converter

--- src/components/AddDependence.vue
+++ src/components/AddDependence.vue
@@ -1,44 +1,23 @@
 <template>
     <div
-        class="p-10 cursor-pointer"
+        style="width: 44px; height: 48px"
+        class="cursor-pointer d-flex justify-content-center align-items-center"
         data-bs-toggle="modal"
         data-bs-target="#exampleModal"
     >
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            class="bi bi-plus-square"
-            viewBox="0 0 16 16"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
             <path
                 d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
             />
-            <path
-                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-            />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
         </svg>
     </div>
-    <div
-        class="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-    >
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="exampleModalLabel">
-                        Добавить валюту в список
-                    </h4>
-                    <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Закрыть"
-                    ></button>
+                    <h4 class="modal-title" id="exampleModalLabel">Добавить валюту в список</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
                 </div>
                 <div class="modal-body">
                     <input
@@ -50,21 +29,8 @@
                     />
                 </div>
                 <div class="modal-footer">
-                    <button
-                        type="button"
-                        class="btn btn-secondary"
-                        data-bs-dismiss="modal"
-                    >
-                        Закрыть
-                    </button>
-                    <button
-                        type="button"
-                        class="btn btn-primary"
-                        @click="addNewDependence"
-                        data-bs-dismiss="modal"
-                    >
-                        Добавить
-                    </button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                    <button type="button" class="btn btn-primary" @click="addNewDependence" data-bs-dismiss="modal">Добавить</button>
                 </div>
             </div>
         </div>
--- src/pages/Converter.vue
+++ src/pages/Converter.vue
@@ -1,23 +1,127 @@
 <template>
-    <div class="w-60">
-        <h1>Калькулятор валют</h1>
-        <div class="d-flex">
-            <input type="text" v-model.number="firstWalletCount" class="" />
+    <div>
+        <h1 class="mb-3">Конвертер валют</h1>
+        <div class="col-8 d-flex justify-content-between">
+            <div class="d-flex flex-wrap">
+                <div class="d-flex align-items-center">
+                    <h4 class="me-2">У меня есть</h4>
+                    <select class="form-select converter__select" v-model="firstWallet" @change="changeWalletsChoise">
+                        <option value="USD" selected>USD</option>
+                        <option v-for="(wallet, i) in walletList" :key="i" :value="wallet">
+                            {{ wallet }}
+                        </option>
+                    </select>
+                    <Add-dependence @addDependence="addNewDependence" />
+                </div>
+                <div
+                    class="w-100 mt-2 border border-1 border-primary p-3 converter__count_block rounded-3 d-flex flex-column justify-content-between"
+                >
+                    <input
+                        @input="(event) => handleWalletCount(event)"
+                        @change="changeFirstMylptiplyer"
+                        type="text"
+                        class="w-100 h-80 border-0 outline-0 fs-1 text-center converter__values"
+                        v-model="firstMultiplyed"
+                    />
+                    <h5 class="m-0">1 {{ firstWallet }} = {{ secondWalletCourse }} {{ secondWallet }}</h5>
+                </div>
+            </div>
+            <div class="converter__transfer col-2 mx-4 mt-4"></div>
+            <div class="d-flex flex-wrap">
+                <div class="d-flex align-items-center">
+                    <h4 class="me-2">Хочу купить</h4>
+                    <select class="form-select converter__select" v-model="secondWallet" @change="changeWalletsChoise">
+                        <option value="BTC" selected>BTC</option>
+                        <option v-for="(wallet, i) in walletList" :key="i" :value="wallet">
+                            {{ wallet }}
+                        </option>
+                    </select>
+                    <Add-dependence @addDependence="addNewDependence" />
+                </div>
+                <div
+                    class="w-100 mt-2 border border-1 border-primary p-3 converter__count_block rounded-3 d-flex flex-column justify-content-between"
+                >
+                    <input
+                        @input="(event) => handleWalletCount(event)"
+                        @change="changeSecondMylptiplyer"
+                        type="text"
+                        class="w-100 h-80 border-0 outline-0 fs-1 text-center converter__values"
+                        v-model="secondMultiplyed"
+                    />
+                    <h5 class="m-0">1 {{ secondWallet }} = {{ firstWalletCourse }} {{ firstWallet }}</h5>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
+import AddDependence from "../components/AddDependence.vue";
 export default {
     data() {
         return {
             walletList: ["RUB", "EUR"],
+            firstWallet: "USD",
+            secondWallet: "BTC",
+            firstWalletCourse: null,
+            secondWalletCourse: null,
+            firstMultiplyed: null,
+            secondMultiplyed: null,
         };
     },
+    components: { AddDependence },
     methods: {
         addNewDependence(newDependence) {
             this.walletList.push(newDependence);
         },
+        handleWalletCount(e) {
+            e.target.value = e.target.value.replace(/[^.\d]+/g, "");
+            if (e.data == "." && e.target.value.split("").filter((elem) => elem == ".").length > 1) {
+                e.target.value = e.target.value.split("").slice(0, e.target.value.split("").lastIndexOf(".")).join("");
+            }
+        },
+        async changeWalletsChoise() {
+            const func = await fetch(
+                `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${this.secondWallet},${this.firstWallet}&tsyms=${this.firstWallet},${this.secondWallet}`
+            );
+            const data = await func.json();
+            this.secondWalletCourse = data[this.firstWallet][this.secondWallet];
+            this.firstWalletCourse = data[this.secondWallet][this.firstWallet];
+            this.secondMultiplyed = data[this.secondWallet][this.secondWallet];
+            this.firstMultiplyed = data[this.secondWallet][this.firstWallet];
+        },
+        async changeSecondMylptiplyer() {
+            this.firstMultiplyed = this.firstWalletCourse * this.secondMultiplyed;
+        },
+        async changeFirstMylptiplyer() {
+            this.secondMultiplyed = this.secondWalletCourse * this.firstMultiplyed;
+        },
+    },
+    async created() {
+        const func = await fetch(
+            `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${this.secondWallet},${this.firstWallet}&tsyms=${this.firstWallet},${this.secondWallet}`
+        );
+        const data = await func.json();
+        this.secondWalletCourse = data[this.firstWallet][this.secondWallet];
+        this.firstWalletCourse = data[this.secondWallet][this.firstWallet];
+        this.secondMultiplyed = data[this.secondWallet][this.secondWallet];
+        this.firstMultiplyed = data[this.secondWallet][this.firstWallet];
     },
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.converter__count_block {
+    height: 200px;
+}
+.converter__select {
+    width: 90px;
+}
+.converter__transfer {
+    background: url("../assets/transfer.svg") no-repeat center;
+}
+.outline-0 {
+    outline: 0;
+}
+.converter__values::placeholder {
+    font-size: 18px;
+}
+</style>
