37763bfa8a9c3f82b2ff6a159332927755aeb86d
Александр Бухарин <buharinzxc123@gmail.com>
2022-06-06 21:06:49 +0500

    idk

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+    <Modal ref="modalBg" />
     <div class="container">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
             <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
@@ -20,6 +21,7 @@
                 <li class="nav-item">
                     <router-link class="nav-link" to="/news">Новости</router-link>
                 </li>
+                <li class="nav-item" @click="showModal">тест хуня</li>
                 <li class="nav-item" v-if="$store.state.userId == null">
                     <router-link class="nav-link" to="/login">Войти</router-link>
                 </li>
@@ -35,8 +37,10 @@
 </template>
 
 <script>
+import Modal from "./components/Modal.vue";
 export default {
     name: "App",
+    components: { Modal },
     methods: {
         handleLogout() {
             this.$store.commit("handleLogout");
---
+++ src/components/Modal.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="modal__background" ref="modalBg">
+        <div class="modal__body">
+            <h3 class="modal__title">Уведомление</h3>
+            <hr class="mt-2" />
+            <p class="modal__text" ref="modalText">Элемент для отображения текста</p>
+            <div class="modal__buttons">
+                <button @click="closeModal" class="btn btn-primary">Закрыть</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        closeModal() {
+            this.$refs.modalBg.classList.add("modal__background-hidden");
+        },
+    },
+};
+</script>
+<style lang="scss">
+.modal {
+    &__background {
+        position: fixed;
+        height: 100vh;
+        width: 100%;
+        background: var(--bs-dark);
+        top: 0;
+        left: 0;
+        z-index: 1000;
+        opacity: 0.9;
+        visibility: visible;
+        transition: all 0.3s ease;
+        &-hidden {
+            opacity: 0;
+            visibility: hidden;
+            transition: all 0.3s ease;
+        }
+    }
+    &__body {
+        background: var(--bs-light);
+        padding: 16px;
+        border-radius: 8px;
+        min-width: 400px;
+        max-width: 600px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -100%);
+    }
+    &__buttons {
+        display: flex;
+        justify-content: flex-end;
+    }
+}
+</style>
