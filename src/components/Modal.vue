f02236ecd46c6962d5d92d8e3b6e828eed9e1fa9
Александр Бухарин <buharinzxc123@gmail.com>
2022-06-10 11:18:13 +0500

    modal fixes

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,7 +2,7 @@
     <div class="modal__background">
         <div class="modal__body">
             <h3 class="modal__title">Уведомление</h3>
-            <hr class="mt-2" />
+            <hr class="mt-2 mb-2" />
             <p class="modal__text">{{ $store.state.modalText }}</p>
             <div class="modal__buttons">
                 <button class="btn btn-primary" @click="$store.commit('closeModal')">Закрыть</button>
@@ -15,15 +15,18 @@ export default {};
 </script>
 <style lang="scss">
 .modal {
+    &__title {
+        font-size: 2.2rem !important;
+    }
     &__background {
         position: fixed;
         height: 100vh;
         width: 100%;
-        background: var(--bs-dark);
+        background: rgba(0, 0, 0, 0.903);
         top: 0;
         left: 0;
         z-index: 1000;
-        opacity: 0.9;
+        opacity: 1;
     }
     &__body {
         background: var(--bs-light);
@@ -39,6 +42,9 @@ export default {};
     &__buttons {
         display: flex;
         justify-content: flex-end;
+    }
+    &__text {
+        font-size: 20px;
     }
 }
 </style>
